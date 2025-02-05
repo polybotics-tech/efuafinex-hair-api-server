865b6fa6502e5946cb947d98bbd20ddaa7ce4ad8
polybotics-tech <polyboticstech@gmail.com>
2025-02-05 11:23:12 +0100

    Update user last seen on login

--- v1/subscribers/auth.js
+++ v1/subscribers/auth.js
@@ -1,13 +1,25 @@
 import EventEmitter from "events";
+import { UserModel } from "../models/user.js";
 export const AuthEvent = new EventEmitter();
 
 //--auth event listeners
-AuthEvent.on("login", async (data) => {
-  console.log("event login: ", data);
-  // do stuff
+AuthEvent.on("login", async (args) => {
+  try {
+    //extract user_id from data from args
+    const { data } = args;
+    const { user } = data;
+    const { user_id } = user;
+
+    // update user last seen
+    if (user_id) {
+      await UserModel.update_user_last_seen(user_id);
+    }
+  } catch (error) {
+    logbot.Error(error?.message);
+  }
 });
 
-AuthEvent.on("register", async (data) => {
-  console.log("event register: ", data);
+AuthEvent.on("register", async (args) => {
+  console.log("event register: ", args);
   // do stuff
 });
